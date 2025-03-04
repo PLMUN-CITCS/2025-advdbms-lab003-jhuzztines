05d6c12b491cdcdec495d94816f7ee530304a8fd
Jhustine <galinatojhustine_bsa@plmun.edu.ph>
2025-03-04 14:46:55 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 01

---
+++ create_and_use_db.sql
@@ -0,0 +1,8 @@
+-- Create the UniversityDB database
+CREATE DATABASE IF NOT EXISTS UniversityDB;
+
+-- Use the created database
+USE UniversityDB;
+
+-- Verify active database
+SELECT DATABASE();
---
+++ drop_db.sql
@@ -0,0 +1,2 @@
+-- Drop the UniversityDB database
+DROP DATABASE IF EXISTS UniversityDB;
